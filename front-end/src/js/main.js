78a23ee1835e5dacdfb0293bf190d19390f11e8b
AliaksandrChachura <skif198560@gmail.com>
2021-05-28 13:27:55 +0300

    [dev] make front-end part

---
+++ front-end/src/js/main.js
@@ -0,0 +1,8 @@
+import '../styles/styles.scss';
+
+import '../assets/copenhagen.png';
+import '../assets/founders.png';
+import '../assets/hamburg.png';
+import '../assets/los-angeles.png';
+import '../assets/new-york.png';
+import '../assets/team.jpg';
